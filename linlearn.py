be23df7aa8fffa784a2d3eb48bf8c8c4f320637c
Chris Homan <cmh@CSs-MacBook-Pro.local>
2020-11-09 07:50:58 -0500

    Week 13 repo.

---
+++ linlearn.py
@@ -0,0 +1,6 @@
+## Read data (20 points)
+
+## Determine value of the 40 linear parameters w0, ..., w39, by discovering the values that minimize the mean squared error between the true values (yi) and the predicted values w0xi0 + ... + w39xi39. You may use analysis or gradient descent (i.e., hill climbing, or descending in this case) (60 points)
+
+## Output answers as params.csv. This should have two rows: one for the labels  w0, ..., w39 and one for the values (20 points)
+
